8fc67c00027478f6a03991f6891da16ac15c61ed
Артем Лединський <ledinskyi.artem@chnu.edu.ua>
2023-03-13 22:53:31 +0200

    майже готова лабораторна

--- OOPLab2Tpl/OOPLab2Tpl.cpp
+++ OOPLab2Tpl/OOPLab2Tpl.cpp
@@ -6,14 +6,31 @@ using namespace std;
 #include "Tasks.h"
 #include "Examples.h"
 
-
+/* task1() {
+    setlocale(LC_CTYPE, "ukr");
+    int a, b, c, d, x, y;
+    cout << "Enter a:";
+    cin >> a;
+    cout << "Enter b:";
+    cin >> b;
+    cout << "Enter c:";
+    cin >> c;
+    cout << "Enter d:";
+    cin >> d;
+    cout << "a*34+((13*a+12*c)/64))-b*2560+d*127-c*18" << endl;
+    y = a * 34 + ((13 * a + 12 * c) / 64) - b * 2560 + d * 127 - c * 18;
+    x = ((a << 1) + (a << 5)) + ((((a << 3) + ((a << 2) + a)) + (c << 3) + (c << 2)) >> 6) - ((b << 12) - (b << 11) + (b << 9)) + ((d << 7) - d) - ((c << 4) + (c << 1));
+    cout << x << " За допомогою операцiй зсуву " << endl;
+    cout << y << " Без операцiй зсуву " << endl;
+    return x;
+}*/
 /// @brief 
 /// @return 
 int main()
 {
     cout << "OOP. Template for laboratory work #2.\n";
 
-    char ch = '5';
+    /*char ch = '5';
     do {
         system("cls");
         MenuTask();
@@ -32,7 +49,16 @@ int main()
         cout << " Press any key and enter\n";
         ch = cin.get();
     } while (ch != '6');
-
+    */
+    cout << "Choose Task which one do you want to watch :";
+    int c;
+    cin >> c;
+    if (c == 1) {
+        task1();
+    }
+    else if (c == 2) {
+        task2();
+    }
     return 0;
 }
 
--- OOPLab2Tpl/Tasks.cpp
+++ OOPLab2Tpl/Tasks.cpp
@@ -13,32 +13,72 @@ void MenuTask()
     cout << "    6.  Exit \n";
 }
 void task1() {
-    // Обчислення виразів з використанням побітових операцій
-    // Calculation of expressions using bitwise operations 
-    cout << " Calculation of expressions using bitwise operations  \n";
+    
+        setlocale(LC_CTYPE, "ukr");
+        int a, b, c, d, x, y;
+        cout << "Enter a:";
+        cin >> a;
+        cout << "Enter b:";
+        cin >> b;
+        cout << "Enter c:";
+        cin >> c;
+        cout << "Enter d:";
+        cin >> d;
+        cout << "a*34+((13*a+12*c)/64))-b*2560+d*127-c*18" << endl;
+        y = a * 34 + ((13 * a + 12 * c) / 64) - b * 2560 + d * 127 - c * 18;
+        x = ((a << 1) + (a << 5)) + ((((a << 3) + ((a << 2) + a)) + (c << 3) + (c << 2)) >> 6) - ((b << 12) - (b << 11) + (b << 9)) + ((d << 7) - d) - ((c << 4) + (c << 1));
+        cout << x << " За допомогою операцiй зсуву " << endl;
+        cout << y << " Без операцiй зсуву " << endl;
+        return ;
 }
 
 void task2()
 {
-    // Шифрування даних з використання побітових операцій 
-    // Data encryption using bitwise operations
     cout << " Data encryption using bitwise operations  \n";
+	int i, n, j;
+	char Text[4][16];
+	char Result[4][16];
+	char g[25];
+	for (i = 0; i < 4; i++) {
+		cout << "Enter Text :";
+		cin >> g;
+		for (j = 0; j < 16; j++) {
+			if (g[i] == NULL) {
+				g[j] = ' ';
+			}
+			Text[i][j] = g[j];
+		}
+	}
+	unsigned short z, x, c, sf, pa;
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 16; j++) {
+			z = i;
+			x = j << 2;
+			z |= x;
+			x = Text[i][j];
+			x <<= 6;
+			z |= x;
+			c = 0;
+			sf = 1;
+			for (pa = 0; pa < 15; pa++) {
+				if (z & sf) {
+					c = 1;
+				}
+				sf <<= 1;
+			}
+			x = 1 << 15;
+			if (c == 1) {
+				z |= x;
+			}
+			Result[i][j] = z;
+		}
+	}
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 16; j++) {
 
-}
-
-void task3()
-{
-    // Шифрування даних з використання стуктур з бітовими полями 
-    // Data encryption using structures with bit fields
-    cout << "  Data encryption using structures with bit fields \n";
-}
-
-
-void task4()
-{   // Задача із використання побітових операцій
-    // The problem of using bitwise operations
-    cout << " Data encryption using structures with bit fields \n";
-
+		}
+	}
+	return;
 }
 
 
